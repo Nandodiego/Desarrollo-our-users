7cdd5805453565d564571c2e4f947c2cb3d87e0f
Nandodiego <cgjacgdl@gmail.com>
2021-11-24 14:11:51 -0500

    Terminado

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,19 +3,27 @@ const main = document.querySelector('#content');
 
 const getInformation = async () => {
     try{
-        const respuesta = await  fetch(API_URL)
+        const respuesta = await  fetch(API_URL);
         const datos = await respuesta.json();
 
         for(let i = 0; i<datos.results.length; i++){
-            main.innerHTML += `<div class="div_users"> <img class="images" id="image" src="${datos.results[i].picture.large}" alt="">
-            <p class="names" id="name"> ${datos.results[i].name.title} ${datos.results[i].name.first} ${datos.results[i].name.last}</p>
-            <p class="cells" id="cell"> ${datos.results[i].cell}</p>
-            <p id="email"> ${datos.results[i].email}</p> </div>`;
-
+            main.innerHTML += 
+            `<div class="div_users"> 
+                <img class="images" id="image" src="${datos.results[i].picture.large}" alt="one user">
+                <p class="names" id="name">
+                    ${datos.results[i].name.title} ${datos.results[i].name.first} ${datos.results[i].name.last}
+                </p>
+                <p class="cells" id="cell">
+                    ${datos.results[i].cell}
+                </p>
+                <p id="email">
+                    ${datos.results[i].email}
+                </p> 
+            </div>`;
         };
         
     } catch(error){
-        console.log(error);
+        alert(error);
     };
 };
 
